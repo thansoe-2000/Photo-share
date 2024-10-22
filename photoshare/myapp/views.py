9c4dc3c390e0c54e1944d9873fb7fb8397098be0
Student <tt6289216@gmail.com>
2024-10-21 19:20:27 -0700

    category

--- photoshare/myapp/views.py
+++ photoshare/myapp/views.py
@@ -4,8 +4,13 @@ from . forms import *
 # Create your views here.
 
 def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
     categories = Category.objects.all()
-    photos = Photo.objects.all()
+    # photos = Photo.objects.all()
     context = {
         'categories':categories, 
         'photos':photos,
@@ -21,6 +26,24 @@ def viewPhoto(request, pk):
 
 def addPhoto(request):
     categories = Category.objects.all()
+    if request.method == 'POST':
+        data = request.POST
+        photo = request.FILES.get('photo')
+        
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+        photo = Photo.objects.create(
+            category = category,
+            description = data['description'],
+            photo = photo,
+            
+        )
+        return redirect(gallery)
+        
     context = {
         'categories':categories,
     }
